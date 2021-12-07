42fa5432167c311cc93bed845369d34fd220c9ec
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-12-08 05:41:46 +0900

    add missing property to response

--- internal/alert/response.go
+++ internal/alert/response.go
@@ -15,18 +15,18 @@ type AlertsResponse struct {
 }
 
 type Alert struct {
-	Slug      string    `json:"slug"`
-	Title     string    `json:"title"`
-	Body      string    `json:"body"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-	Account   Account   `json:"account"`
-}
-
-type Account struct {
-	Username string `json:"username"`
-	Bio      string `json:"bio"`
-	Image    string `json:"image"`
+	Slug           string    `json:"slug"`
+	Title          string    `json:"title"`
+	Body           string    `json:"body"`
+	PairAddress    string    `json:"pairAddress"`
+	AlertType      string    `json:"alertType"`
+	AlertValue     string    `json:"alertValue"`
+	AlertOption    string    `json:"alertOption"`
+	ExpirationTime time.Time `json:"expirationTime"`
+	AlertActions   string    `json:"alertActions"`
+	AlertStatus    string    `json:"alertStatus"`
+	CreatedAt      time.Time `json:"createdAt"`
+	UpdatedAt      time.Time `json:"updatedAt"`
 }
 
 // NewAlertsResponse converts alert models and total count to AlertsResponse
@@ -46,11 +46,18 @@ func NewAlertsResponse(alerts []*model.Alert, total int64) *AlertsResponse {
 func NewAlertResponse(a *model.Alert) *AlertResponse {
 	return &AlertResponse{
 		Alert: Alert{
-			Slug:      a.Slug,
-			Title:     a.Title,
-			Body:      a.Body,
-			CreatedAt: a.CreatedAt,
-			UpdatedAt: a.UpdatedAt,
+			Slug:           a.Slug,
+			Title:          a.Title,
+			Body:           a.Body,
+			PairAddress:    a.PairAddress,
+			AlertType:      a.AlertType,
+			AlertValue:     a.AlertValue,
+			AlertOption:    a.AlertOption,
+			ExpirationTime: a.ExpirationTime,
+			AlertActions:   a.AlertActions,
+			AlertStatus:    a.AlertStatus,
+			CreatedAt:      a.CreatedAt,
+			UpdatedAt:      a.UpdatedAt,
 		},
 	}
 }
