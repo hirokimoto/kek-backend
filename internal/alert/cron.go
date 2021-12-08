05128c00d94effa7d8f5674123f6f6d17349fb23
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-12-09 07:43:17 +0900

    add synchronization between go routines

--- internal/alert/cron.go
+++ internal/alert/cron.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"sync"
 
 	"kek-backend/internal/uniswap"
 
@@ -43,30 +44,42 @@ func sendMessage() {
 func StartCron() {
 	c := cron.New(cron.WithSeconds())
 	c.AddFunc("@every 5s", func() {
-		c1 := make(chan string)
-		c2 := make(chan string)
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		ch := make(chan int)
 
 		var ethPrice float64
 		var tokenPrice float64
-		query1 := uniswap.QueryBundles()
-		go uniswap.Request(query1, c1)
-		query2 := uniswap.QuertyToken("0x2f02be0c4021022b59e9436f335d69df95e5222a")
-		go uniswap.Request(query2, c2)
 
-		select {
-		case msg1 := <-c1:
+		go func() {
+			c1 := make(chan string, 1)
+			query1 := uniswap.QueryBundles()
+			uniswap.Request(query1, c1)
+
+			msg1 := <-c1
 			var bundles uniswap.Bundles
 			json.Unmarshal([]byte(msg1), &bundles)
 			ethPrice, _ = strconv.ParseFloat(bundles.Data.Bundles[0].EthPrice, 64)
-			fmt.Println("TK1: ", ethPrice, tokenPrice)
-		case msg2 := <-c2:
+			ch <- 1
+			wg.Done()
+		}()
+
+		go func() {
+			<-ch
+			c2 := make(chan string, 1)
+			query2 := uniswap.QuertyToken("0x2f02be0c4021022b59e9436f335d69df95e5222a")
+			uniswap.Request(query2, c2)
+
+			msg2 := <-c2
 			var tokens uniswap.Tokens
 			json.Unmarshal([]byte(msg2), &tokens)
 			tokenPrice, _ = strconv.ParseFloat(tokens.Data.Tokens[0].DerivedETH, 64)
-			fmt.Println("TK2: ", ethPrice, tokenPrice)
-		}
+			wg.Done()
+		}()
 
-		fmt.Println("$$$$$:   ", ethPrice, tokenPrice)
+		wg.Wait()
+		fmt.Println("$$$$$:   ", ethPrice*tokenPrice)
 		// go sendMessage()
 	})
 	c.Start()
